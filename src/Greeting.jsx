f8010a6383bd05a6a98702867de731137f7357ab
Anurag Augustin Bilung <33390217+anurag-augustin@users.noreply.github.com>
2022-04-24 00:27:42 +0530

    h tag fix

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -5,9 +5,9 @@ const Greeting = (props) => {
       <>
     <div className="bod">
         <div className="box">
-          <h1 className="heading">
+          <h3 className="heading">
             Hello Anurag, <span style={props.cssStyle}>{props.greeting}</span>
-          </h1>
+          </h3>
         </div>
       </div>
       </>
